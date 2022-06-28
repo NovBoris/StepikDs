d6de793d29fa30e4296b84f8bac9431f28ffe650
NovBoris <novosad.b@mail.ru>
2022-06-28 12:17:04 +0300

    Тест commit в PyCharm

---
+++ StepikDs2.4.py
@@ -0,0 +1,6 @@
+# Вам дается текстовый файл, содержащий некоторое количество непустых строк.
+# На основе него сгенерируйте новый текстовый файл, содержащий те же строки в обратном порядке.
+#
+# with open('dataset_24465_4.txt') as text, open('dataset_24465_4_copy.txt', 'w') as text_revers:
+#     x = text.readlines()
+#     text_revers.writelines(reversed(x))
