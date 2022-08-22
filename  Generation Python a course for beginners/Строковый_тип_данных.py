6032629a712ebcf90b39194ba00b01f95d118add
Boris Novosad <novosad.b@mail.ru>
2022-08-22 15:25:48 +0300

    6.2 Строковый тип данных

---
+++  Generation Python a course for beginners/Строковый_тип_данных.py
@@ -0,0 +1,43 @@
+# print('''"Python is a great language!", said Fred. "I don't ever remember having this much fun before."''')
+
+
+# name = input()
+# lastname = input()
+# print(f'Hello {name} {lastname}! You just delved into Python')
+
+# command = input()
+# print(f'Футбольная команда {command} имеет длину {len(command)} символов')
+
+
+# a, b, c = input(), input(), input()
+# d = sorted([a, b, c], key=len)
+# print(d[0])
+# print(d[2])
+
+
+# a, b, c = len(input()), len(input()), len(input())
+# if (2 * b - c - a) * (2 * c - b - a) * (2 * a - b - c) == 0:
+#     print('YES')
+# else:
+#     print('NO')
+
+
+# a = input()
+# if 'синий' in a:
+#     print('YES')
+# else:
+#     print('NO')
+
+
+# a = input()
+# if 'суббота' in a or 'воскресенье' in a:
+#     print('YES')
+# else:
+#     print('NO')
+
+
+# a = input()
+# if '@' in a and '.' in a:
+#     print('YES')
+# else:
+#     print('NO')
