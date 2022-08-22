016784c1719626e0ad51df6fb94d3f9c86b95188
Boris Novosad <novosad.b@mail.ru>
2022-08-22 14:49:44 +0300

    6.1 Числовые типы данных: int, float

---
+++  Generation Python a course for beginners/Числовые_типы_данных+int_float.py
@@ -0,0 +1,70 @@
+# a, b = float(input()), float(input())
+# S = 0.5 * a * b
+# print(S)
+
+
+# c, b, a = float(input()), float(input()), float(input())
+# V = a + b
+# T = c / V
+# print(T)
+
+
+# a = float(input())
+# if a == 0:
+#     print('Обратного числа не существует')
+# else:
+#     print(1/a)
+
+
+# a = float(input())
+# C = 5 / 9 * (a - 32)
+# print(C)
+
+
+# a = float(input())
+# if a <= 2:
+#     print(a * 10.5)
+# else:
+#     print(21 + (a - 2) * 4)
+
+
+# a = input().split('.')
+# print(a[1][0])
+
+
+# a = float(input())
+# while a > 1:
+#     a -= 1
+# print(a)
+
+# print(float(input()) % 1)
+
+
+# num = max(1, 3, -5, 7) + min(-3, 6, -8, -1) + abs(-17)
+# print(num)
+
+
+# a, b, c, d, e = int(input()), int(input()), int(input()), int(input()), int(input())
+# print(f'Наименьшее число = {min(a, b, c, d, e)}\n'
+#       f'Наибольшее число = {max(a, b, c, d, e)}')
+
+
+# a, b, c = int(input()), int(input()), int(input())
+# d = sorted([a, b, c], reverse=True)
+# for i in d:
+#     print(i)
+
+
+# a = input()
+# if int(max(a)) - int(min(a)) == int(a.replace(max(a), '').replace(min(a), '')):
+#     print('Число интересное')
+# else:
+#     print('Число неинтересное')
+
+
+# a, b, c, d, e = float(input()), float(input()), float(input()), float(input()), float(input())
+# print(abs(a) + abs(b) + abs(c) + abs(d) + abs(e))
+
+
+# a, b, c, d = int(input()), int(input()), int(input()), int(input())
+# print(abs(a - c) + abs(b - d))
