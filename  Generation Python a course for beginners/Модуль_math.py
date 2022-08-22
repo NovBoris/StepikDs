337e4e51e8c4e1785c4a559efa6ecaa62dcece51
Boris Novosad <novosad.b@mail.ru>
2022-08-22 16:30:55 +0300

    6.3 Модуль math

---
+++  Generation Python a course for beginners/Модуль_math.py
@@ -0,0 +1,50 @@
+import math
+
+# a, b, c, d = float(input()), float(input()), float(input()), float(input())
+# print(math.sqrt(math.pow(a - c, 2) + math.pow(b - d, 2)))
+
+
+# R = float(input())
+# S = math.pi * math.pow(R, 2)
+# C = 2 * math.pi * R
+# print(S)
+# print(C)
+
+
+# a, b = float(input()), float(input())
+# print((a + b) / 2)
+# print(math.sqrt(a * b))
+# print((2 * a * b) / (a + b))
+# print(math.sqrt((math.pow(a, 2) + math.pow(b, 2)) / 2))
+
+
+# a = float(input())
+# # r = (a * math.pi) / 180
+# r = math.radians(a)
+# print(math.sin(r) + math.cos(r) + math.pow(math.tan(r), 2))
+
+
+# a = float(input())
+# print(math.ceil(a) + math.floor(a))
+
+
+# a, b, c = float(input()), float(input()), float(input())
+# D = math.pow(b, 2) - (4 * a * c)
+# if D > 0:
+#     x = (-b - math.sqrt(D)) / (2 * a)
+#     x1 = (-b + math.sqrt(D)) / (2 * a)
+#     if x > x1:
+#         print(x1)
+#         print(x)
+#     else:
+#         print(x)
+#         print(x1)
+# elif D == 0:
+#     print(-b / (2 * a))
+# else:
+#     print('Нет корней')
+
+
+# n, a = int(input()), float(input())
+# S = (n * math.pow(a, 2) / (4 * math.tan(math.pi / n)))
+# print(S)
