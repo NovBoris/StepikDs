9c9761bb5f921f8d5ef00b77a35232038330d551
Boris Novosad <novosad.b@mail.ru>
2022-08-23 15:02:49 +0300

    7.2 Цикл for: функция range

---
+++  Generation Python a course for beginners/Цикл_for_функция_range.py
@@ -0,0 +1,32 @@
+# m, n = int(input()), int(input())
+# for i in range(m, n + 1):
+#     print(i)
+
+
+# m, n = int(input()), int(input())
+# if m <= n:
+#     for i in range(m, n + 1):
+#         print(i)
+# else:
+#     for i in range(m, n - 1, -1):
+#         print(i)
+
+
+# m, n = int(input()), int(input())
+# if m % 2 == 0:
+#     for i in range(m - 1, n - 1, -2):
+#         print(i)
+# else:
+#     for i in range(m, n - 1, -2):
+#         print(i)
+
+
+# m, n = int(input()), int(input())
+# for i in range(m, n + 1):
+#     if i % 17 == 0 or i % 10 == 9 or i % 3 == 0 and i % 5 == 0:
+#         print(i)
+
+
+# n = int(input())
+# for i in range(1, 11):
+#     print(f'{n} x {i} = {n * i}')
