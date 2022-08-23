56ce9fb5839cd11881d2e0f2d8f16cb135dd697e
Boris Novosad <novosad.b@mail.ru>
2022-08-23 14:31:48 +0300

    7.1 Цикл for

---
+++  Generation Python a course for beginners/Цикл_for.py
@@ -0,0 +1,45 @@
+# for i in range(10):
+#     print('Python is awesome!')
+
+
+# a, b = input(), int(input())
+# for i in range(b):
+#     print(a)
+
+
+# for i in range(6):
+#     print('AAA')
+# for i in range(5):
+#     print('BBBB')
+# else:
+#     print('E')
+# for i in range(9):
+#     print('TTTTT')
+# else:
+#     print('G')
+
+
+# a = int(input())
+# for i in range(a):
+#     print('*' * 20)
+
+
+# a = input()
+# for i in range(10):
+#     print(f'{i} {a}')
+
+
+# a = int(input())
+# for i in range(a + 1):
+#     print(f'Квадрат числа {i} равен {i ** 2}')
+
+
+# a = int(input())
+# for i in range(a, 0, -1):
+#     print('*' * i)
+
+
+# m, p, n = float(input()), int(input()), int(input())
+# for i in range(n):
+#     print(i + 1, m)
+#     m = m + ((m / 100) * p)
