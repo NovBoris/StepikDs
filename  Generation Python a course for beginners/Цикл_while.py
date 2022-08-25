e11c76731b81754ef317031857fe4d0a98061565
Boris Novosad <novosad.b@mail.ru>
2022-08-25 17:40:46 +0300

    7.4 Цикл while

---
+++  Generation Python a course for beginners/Цикл_while.py
@@ -0,0 +1,70 @@
+# i = 7
+# a = 5
+# while i < 11:
+#     a += i
+#     i += 2
+# print(a)
+
+
+# while True:
+#     word = input()
+#     if word == 'КОНЕЦ':
+#         break
+#     print(word)
+
+
+# while True:
+#     word = input()
+#     if word.upper() == 'КОНЕЦ':
+#         break
+#     print(word)
+
+
+# count = 0
+# while True:
+#     word = input()
+#     if word in ['стоп', 'хватит', 'достаточно']:
+#         break
+#     count += 1
+# print(count)
+
+
+# num = int(input())
+# while num % 7 == 0:
+#     print(num)
+#     num = int(input())
+
+
+# num = int(input())
+# res = 0
+# while num >= 0:
+#     res += num
+#     num = int(input())
+# print(res)
+
+
+# num = int(input())
+# res = 0
+# while 0 <= num <= 5:
+#     if num == 5:
+#         res += 1
+#     num = int(input())
+# print(res)
+
+
+# num = int(input())
+# res = 0
+# while num > 0:
+#     if num // 25 != 0:
+#         res += num // 25
+#         num -= 25 * (num // 25)
+#     elif num // 10 != 0:
+#         res += num // 10
+#         num -= 10 * (num // 10)
+#     elif num // 5 != 0:
+#         res += num // 5
+#         num -= 5 * (num // 5)
+#     elif num // 1 != 0:
+#         res += num // 1
+#         num -= 1 * (num // 1)
+# print(res)
