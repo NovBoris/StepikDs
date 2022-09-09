2c213c36f07cb72041eceeddc15b389158d64560
Boris Novosad <novosad.b@mail.ru>
2022-09-09 13:00:01 +0300

    7.6 break, continue и else

---
+++  Generation Python a course for beginners/break_continue_и_else.py
@@ -0,0 +1,17 @@
+# n = int(input())
+# for i in range(2, n + 1):
+#     if n % i == 0:
+#         print(i)
+#         break
+
+
+# n = int(input())
+# for i in range(1, n + 1):
+#     if i in range(5, 10):
+#         continue
+#     if i in range(17, 38):
+#         continue
+#     if i in range(78, 88):
+#         continue
+#     print(i)
+
